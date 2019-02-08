69ee776ac89cd12f8ba0d0c8013f4f52a71b9e77
Ann <rraazznoe@gmail.com>
2019-02-08 03:24:52 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 def display_rainbow(r_c)
-  puts "R: #{r_c[0]}, O: #{r_c[1]}, Y: #{[2]}, G: #{[3]}, 
+  puts "R: #{r_c[0]}, O: #{r_c[1]}, Y: #{[2]}, G: #{[3]},
   B: #{[4]}, I: #{[5]}, V: #{[5]}""
 end
 
